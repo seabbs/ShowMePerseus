4d662dc3a4dc86ec5119142a8031e35d70d226ab
seabbs <s.e.abbott12@gmail.com>
2017-12-07 23:18:18 +0000

    basic app framework complete

--- utils.R
+++ utils.R
@@ -5,24 +5,3 @@ get_lemmatized_greek_text <- function(urn) {
   r <- xml2::read_xml(url)
   xml2::as_list(r)
 }
-
-filter_list <- function(text_list, excerpt) {
-  if (grepl("-", excerpt)) {
-    p <- strsplit(excerpt, "-")[[1]]
-    p <- strsplit(p, "\\.")
-    p1_c <- p[[1]][1]
-    p1_v <- as.numeric(p[[1]][2])
-    p2_c <- p[[2]][1]
-    p2_v <- as.numeric(p[[2]][2])
-    if (identical(p1_c, p2_c)) {
-      vv <- paste0(p1_c, ".", p1_v:p2_v)
-    } else {
-      vv <- purrr::map(p1_c:p2_c, paste0, ".", 1:99) %>%
-        purrr::flatten_chr()
-    }
-  } else {
-    vv <- excerpt
-  }
-  purrr::flatten(text_list) %>%
-    purrr::keep(~attr(.x, "p") %in% vv)
-}
